ec379f827f6de3e32de625e2c3e5e53e040a6d72
maxgit <rotvel@gmail.com>
2023-05-23 08:23:42 +0200

    Missing file in previous commit

--- Source/MetalArchivesCore/Models/Responses/SearchResponse.cs
+++ Source/MetalArchivesCore/Models/Responses/SearchResponse.cs
@@ -1,14 +1,26 @@
-﻿namespace MetalArchivesCore.Models.Responses
+﻿using System.Text.Json.Serialization;
+
+namespace MetalArchivesCore.Models.Responses
 {
     /// <summary>
     /// Ajax response from server
     /// </summary>
-    class SearchResponse
+    class SearchResponse<T>
     {
         public string error { get; set; }
+     
         public int iTotalRecords { get; set; }
+        
         public int iTotalDisplayRecords { get; set; }
+        
         public int sEcho { get; set; }
+        
         public string[][] aaData { get; set; }
+
+        /// <summary>
+        /// Deserialized aaData
+        /// </summary>
+        [JsonIgnore]
+        public List<T> Items { get; set; }
     }
 }
